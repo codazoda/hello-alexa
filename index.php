2db30074d1cda6d2264a4d1dcf418fbac7567152
Joel Dare <joel@joeldare.com>
2018-02-09 11:56:34 -0700

    removed file logging for a moment

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $postData = file_get_contents( 'php://input' );
 $alexaRequest = json_decode( $post );
 
 // Throw it in a debug log file that anyone can view
-file_put_contents('./debug.log', $alexaRequest);
+//file_put_contents('./debug.log', $alexaRequest);
 
 // Send some output
 $result = array(
